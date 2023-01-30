f088e4b9008f54e0a214a9c2b9e84b6ce0ee67bb
Remco <remcotaal@hotmail.com>
2023-01-30 15:32:14 +0100

    Add day1 part1

---
+++ src/main/kotlin/day1/part1/main.kt
@@ -0,0 +1,29 @@
+package day1.part1
+
+import java.io.File
+import java.util.concurrent.atomic.AtomicInteger
+
+fun main(args: Array<String>) {
+    print(determineMostCalories("day1.txt"))
+}
+
+fun determineMostCalories(fileName : String): Int {
+    var currentCalories = 0
+    var mostCalories = 0
+    File(fileName).forEachLine {
+        currentCalories = if (it != "") {
+            currentCalories.plus(it.toInt())
+        } else {
+            mostCalories = checkMostCalories(currentCalories, mostCalories)
+            0
+        }
+    }
+    return mostCalories
+}
+
+fun checkMostCalories(currentCalories : Int, mostCalories: Int): Int {
+    if (currentCalories > mostCalories) {
+        return currentCalories
+    }
+    return mostCalories
+}

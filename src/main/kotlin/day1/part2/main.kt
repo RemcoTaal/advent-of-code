00c613b7baf4e190de1f4b14631c0b83c7a249e4
RemcoTaal <remco.taal@student.hu.nl>
2023-02-20 17:01:42 +0100

    Add day1.txt and day1.part2

---
+++ src/main/kotlin/day1/part2/main.kt
@@ -0,0 +1,21 @@
+package day1.part2
+
+import java.io.File
+
+fun main(args: Array<String>) {
+    print(determineTopThree("day1.txt"))
+}
+
+fun determineTopThree(fileName: String): Int {
+    val calories = mutableListOf<Int>()
+    var currentCalories = 0
+    File(fileName).forEachLine { calorie ->
+        currentCalories = if (calorie != "") {
+            currentCalories.plus(calorie.toInt())
+        } else {
+            calories.add(currentCalories)
+            0
+        }
+    }
+    return calories.sortedByDescending { it }.subList(0, 3).sum()
+}

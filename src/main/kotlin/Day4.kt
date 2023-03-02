5728c6adec42277f1e81c2d660fc43ba308f5511
RemcoTaal <remco.taal@student.hu.nl>
2023-03-02 09:34:57 +0100

    Update previous days

--- src/main/kotlin/Day4.kt
+++ src/main/kotlin/Day4.kt
@@ -16,13 +16,13 @@ class Day4 : Day("day4") {
         return getResult(false)
     }
 
-    private fun getResult(useInfixContains: Boolean): Int {
+    private fun getResult(useInfixOverlaps: Boolean): Int {
         var contained = 0
         file.readLines().forEach { line ->
             val ranges = line.split(",", "-").map { it.toInt() }
             val range1 = ranges[0]..ranges[1]
             val range2 = ranges[2]..(ranges[3])
-            if (useInfixContains) {
+            if (useInfixOverlaps) {
                 if (range1 overlaps range2 || range2 overlaps range1) {
                     contained += 1
                 }

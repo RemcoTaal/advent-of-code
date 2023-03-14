cc08918afcc3714c927aeb246daa915c219f775e
RemcoTaal <remco.taal@student.hu.nl>
2023-03-14 14:52:51 +0100

    Add day 8 part 2

--- src/main/kotlin/Day8.kt
+++ src/main/kotlin/Day8.kt
@@ -7,10 +7,14 @@ class Day8 : Day("day8") {
         return getTotalVisibleTrees()
     }
 
+    override fun executePartTwo(): Int {
+        return getMaxScenicScore()
+    }
+
     private fun getTotalVisibleTrees(): Int {
-        val leftToRight = grid.content.sortedBy { it.coordinates.y }.chunked(grid.maxX)
+        val leftToRight = grid.content.sortedBy { it.coordinates.y }.chunked(grid.maxX + 1)
         val rightToLeft = leftToRight.map { it.reversed() }
-        val topToBottom = grid.content.sortedBy { it.coordinates.x }.chunked(grid.maxY)
+        val topToBottom = grid.content.sortedBy { it.coordinates.x }.chunked(grid.maxY + 1)
         val bottomToTop = topToBottom.map { it.reversed() }
 
         val visibleTrees = getVisibleTrees(leftToRight) + getVisibleTrees(rightToLeft) + getVisibleTrees(topToBottom) + getVisibleTrees(bottomToTop)
@@ -31,16 +35,29 @@ class Day8 : Day("day8") {
         return visibleTrees
     }
 
+    private fun getMaxScenicScore(): Int {
+        // Skip edges because max score will always be 0
+        return grid.content.filter { it.coordinates.x != 0 && it.coordinates.x != grid.maxX && it.coordinates.y != 0 && it.coordinates.y != grid.maxY }
+            .maxOf { tree ->
+                val horizontalRow = grid.content.filter { it.coordinates.y == tree.coordinates.y }.sortedBy { it.coordinates.x }
+                val verticalRow = grid.content.filter { it.coordinates.x == tree.coordinates.x }.sortedBy { it.coordinates.y }
 
+                // Use lastOrNull to get the blocking tree with the highest index
+                val upScenicScore = verticalRow.lastOrNull { it.coordinates.y < tree.coordinates.y && it.value >= tree.value }?.let { tree.coordinates.y - it.coordinates.y } ?: tree.coordinates.y
+                val leftScenicScore = horizontalRow.lastOrNull { it.coordinates.x < tree.coordinates.x && it.value >= tree.value }?.let { tree.coordinates.x - it.coordinates.x } ?: tree.coordinates.x
 
-    override fun executePartTwo(): Int {
-        return 0
+                // Use firstOrNull to get the blocking tree with the lowest index
+                val rightScenicScore = horizontalRow.firstOrNull { it.coordinates.x > tree.coordinates.x && it.value >= tree.value }?.let { it.coordinates.x - tree.coordinates.x } ?: (grid.maxX - tree.coordinates.x)
+                val downScenicScore = verticalRow.firstOrNull { it.coordinates.y > tree.coordinates.y && it.value >= tree.value }?.let { it.coordinates.y - tree.coordinates.y } ?: (grid.maxY - tree.coordinates.y)
+
+                upScenicScore * leftScenicScore * rightScenicScore * downScenicScore
+            }
     }
 
     class Grid(val file: File) {
         val content: List<Tree> = setupGrid()
-        val maxX = content.maxOf { it.coordinates.x } + 1
-        val maxY = content.maxOf { it.coordinates.y } + 1
+        val maxX = content.maxOf { it.coordinates.x }
+        val maxY = content.maxOf { it.coordinates.y }
 
         private fun setupGrid(): List<Tree> {
             return file.readLines().flatMapIndexed { y, line ->

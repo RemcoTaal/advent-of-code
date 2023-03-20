00c249be633ec3a1700c833cbcf5edcfaee046f3
RemcoTaal <remco.taal@student.hu.nl>
2023-03-20 16:50:59 +0100

    Improve performance by reducing input during runtime

--- src/main/kotlin/Day6.kt
+++ src/main/kotlin/Day6.kt
@@ -1,9 +1,11 @@
-import java.nio.CharBuffer
-
 class Day6 : Day("day6") {
 
     private fun List<Byte>.onlyUnique(): Boolean {
         return this.size == this.toSet().size
+    }
+
+    private fun List<Byte>.getIndexOfLatestRepeatedByte(): Int {
+        return this.slice(1 until lastIndex).indexOf(this.last())
     }
 
     override fun executePartOne(): Int {
@@ -16,10 +18,17 @@ class Day6 : Day("day6") {
 
     private fun getResult(windowSize: Int): Int {
         val bytes = file.readBytes()
-        bytes.indices.forEach { i ->
-            if (bytes.slice(i until i+windowSize).onlyUnique()) return i+windowSize
+        var currentIndex = 0
+        while (true) {
+            val slice = bytes.slice(currentIndex until currentIndex+windowSize)
+            if(slice.onlyUnique()) {
+                return currentIndex+windowSize
+            } else {
+                val indexLatestRepeatedByte = slice.getIndexOfLatestRepeatedByte()
+                if (indexLatestRepeatedByte == -1) currentIndex++ else currentIndex += indexLatestRepeatedByte + 1
+                if (currentIndex+windowSize>bytes.lastIndex) throw InternalError("No unique set of size $windowSize found")
+            }
         }
-        throw InternalError("No unique set of size $windowSize found")
     }
 }
 
